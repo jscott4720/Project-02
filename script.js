c6ad3dd6a4f0535f078e848cb932af545f1d7d09
Jeremiah <jscott4720@gmail.com>
2020-06-28 16:46:24 -0400

    made if statement for one round

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // created player objects with a name and empty deck
-var player1 = {
+let player1 = {
     name: "Player 1",
     deck: [],
   };
@@ -32,7 +32,7 @@ var player1 = {
   //shuffle the deck (this hoe works) done at the beginning of the game using .onload//
 function shuffle(deck) {
     for (
-      var j, x, i = deck.length;
+      let j, x, i = deck.length;
       i;
       j = Math.floor(Math.random() * i),
         x = deck[--i],
@@ -42,7 +42,9 @@ function shuffle(deck) {
     return deck;
   }
   //for loop to hand out the deck(this hoe also works)also done using .onload//
- function splitDeck(deck) {
+
+
+  function splitDeck(deck) {
     for (let i = 0; i < deck.length; i++) {
       if (i < 26) {
         player1.deck.push(deck[i]);
@@ -52,49 +54,69 @@ function shuffle(deck) {
     }
   }
   
-//this is the real bitch... still... because we are using the correct syntax to pull from an array inside an object inside another object (see fruits example) and its still not working....//
+// //this is the real bitch... still... because we are using the correct syntax to pull from an array inside an object inside another object (see fruits example) and its still not working....//
 
 
-//fruits.citrus[0] == the object orange  
-//player1.deck[0] == the first card in the players deck
-//declaring a variable = to the syntax of said object in the console gives the desired result of isolating the specific object
-// the variable 'orange' is declared HERE and is defined in the global scope
-// the variable 'player1Card' is here and is undefined in the global scope
-// WHEN this works the way to get the cards to compare would be declaring a variable 'cardsInPlay', making this an array and putting the variable 
+// //fruits.citrus[0] == the object orange  
+// //player1.deck[0] == the first card in the players deck
+// //declaring a variable = to the syntax of said object in the console gives the desired result of isolating the specific object
+// // the variable 'orange' is declared HERE and is defined in the global scope
+// // the variable 'player1Card' is here and is undefined in the global scope
+// // WHEN this works the way to get the cards to compare would be declaring a variable 'cardsInPlay', making this an array and putting the variable (look at the confidence... and now this hoe works too)
+// 
 
 
-//   function compareCards() {
-//     let cardsInPlay = player1.deck.shift()
-//   }
+// //   function compareCards() {
+// //     let cardsInPlay = player1.deck.shift()
+// //   }
   var fruits = {
-      citrus: [{Orange:
+      citrus: [{orange:
     "tasty"}, "Lime", "Lemon"] 
   }
-
-let orange = fruits.citrus.shift();
-
+ let orange = fruits.citrus.shift();
 
 
 
 
-//   /*function to flip the cards to check the value*/
+number1 = {
+  name : "One",
+  value : 1,
+  array : ["string one"]
+}
+
+number2 = {
+  name : "Two",
+  value : 2,
+  array : ["string two"]
+}
 
 
 
-// //     //if player 1 card value is higher than the player 2 card value, player  1 wins
-// //     if (player1 % 13 > player2 % 13) {
-// //       //pushes current cards from each deck to the back of the player's deck
-// //       player2.deck.pop();
-// //       player1.push();
-// //       //removes current card from the front of each deck
-// //       playerHand.shift();
-// //       compHand.shift();
-// //       //update card counts and check for a winner
-// //       updateCount();
-// //       checkWin();
-// //     }
+    //if player 1 card value is higher than the player 2 card value, player  1 wins
+    if (player1Card.value > player2Card.value) {
+      //player1 keeps their card player2 loses their card and both cards go to the bottom of winning player deck (shuffle?) 
+      cardsInPlay.pop();
+      player1.deck.push(cardsInPlay);
+      //^ this will cause problems because its putting an array inside an array 
+      
 
- window.onload = shuffle(deck); splitDeck(deck); 
+      // //removes current card from the front of each deck
+      // playerHand.shift();
+      // compHand.shift();
+      // //update card counts and check for a winner
+      // updateCount();
+      // checkWin();
+    }
 
 
+shuffle(deck); 
+splitDeck(deck); 
+
+
+ let player1Card = player1.deck.shift()
+
+let player2Card = player2.deck.shift()
+
+
+cardsInPlay = [player1Card, player2Card]
 
